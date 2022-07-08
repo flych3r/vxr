d2818a6173ef3efabd673e257c361e0dc9d22a4a
flych3r <matheus.sampaio011@gmail.com>
2022-07-08 02:54:22 +0000

    :zap: changed model api to transformers

---
+++ vxr/models/configuration.py
@@ -0,0 +1,71 @@
+"""XrayReportGeneration model configuration."""
+
+from __future__ import annotations
+
+import json
+
+from transformers import AutoConfig, PretrainedConfig
+
+
+class XrayReportGenerationConfig(PretrainedConfig):
+    """This is the configuration class to store the configuration."""
+    attribute_map = {
+        'hidden_size': 'dim',
+        'd_model': 'dim'
+    }
+
+    def __init__(
+        self,
+        pretrained_encoder: str = 'google/vit-base-patch16-224-in21k',
+        pretrained_decoder: str = 'google/t5-efficient-base',
+        **kwargs
+    ):
+        """Creates a new configuration.
+
+        Args:
+            pretrained_encoder: encoder model
+            pretrained_decoder: decoder model
+
+        Raises:
+            ValueError:
+                Encoder and decoder model sizes are not compatible.
+        """
+        super().__init__(**kwargs)
+        self.pretrained_encoder = AutoConfig.from_pretrained(pretrained_encoder)
+        self.pretrained_decoder = AutoConfig.from_pretrained(pretrained_decoder)
+        enc_size = self.pretrained_encoder.hidden_size
+        dec_size = self.pretrained_decoder.d_model
+        if enc_size != dec_size:
+            raise ValueError(
+                f'Encoder and Decoder must have same sizes ({enc_size} != {dec_size})'
+            )
+        self.dim = enc_size
+        self.freeze_encoder = False
+        self.is_encoder_decoder = True
+        self.decoder_start_token_id = self.pretrained_decoder.pad_token_id
+
+    def to_json_string(self, use_diff: bool = True) -> str:
+        """Serializes this instance to a JSON string.
+
+        Args:
+            use_diff:
+                If set to `True`, only the difference between the config
+                instance and the default `PretrainedConfig()`
+                is serialized to JSON string.
+
+        Returns:
+            `str`:
+                String containing all the attributes that make up this
+                configuration instance in JSON format.
+        """
+        if use_diff is True:
+            config_dict = self.to_diff_dict()
+        else:
+            config_dict = self.to_dict()
+        config_dict['pretrained_encoder'] = config_dict.pop(
+            'pretrained_encoder'
+        ).to_dict()
+        config_dict['pretrained_decoder'] = config_dict.pop(
+            'pretrained_decoder'
+        ).to_dict()
+        return json.dumps(config_dict, indent=2, sort_keys=True) + '\n'
---
+++ vxr/models/modeling.py
@@ -0,0 +1,84 @@
+"""XrayReportGeneration model."""
+
+from __future__ import annotations
+
+from collections import OrderedDict, UserDict
+
+import torch
+from transformers import AutoModel, AutoModelForSeq2SeqLM, PreTrainedModel
+
+from vxr.models.configuration import XrayReportGenerationConfig
+
+
+class XrayReportGeneration(PreTrainedModel):
+    """Model for generating x-ray reports from images."""
+    config_class = XrayReportGenerationConfig
+    base_model_prefix = 'xrrg'
+
+    def __init__(self, config: XrayReportGenerationConfig):
+        """Create a XrayReportGeneration from configuration.
+
+        Args:
+            config: Model configuration.
+        """
+        super().__init__(config)
+        self.encoder = AutoModel.from_pretrained(
+            self.config.pretrained_encoder.name_or_path
+        )
+        self.decoder = AutoModelForSeq2SeqLM.from_pretrained(
+            self.config.pretrained_decoder.name_or_path
+        )
+        self.main_input_name = self.encoder.main_input_name
+        if self.config.freeze_encoder:
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+        try:
+            del self.model.encoder
+        except AttributeError:
+            pass
+
+    def forward(
+        self,
+        pixel_values: torch.FloatTensor | UserDict = None,
+        labels: torch.LongTensor = None,
+        encoder_outputs: OrderedDict = None,
+        decoder_input_ids: torch.LongTensor = None,
+        **kwargs
+    ) -> torch.LongTensor | OrderedDict:
+        """Forward pass on model.
+
+        Args:
+            pixel_values:
+                Image pixel values
+            labels:
+                Labels for computing the sequence classification/regression loss.
+            encoder_outputs:
+                Hidden states at the output of the last layer of the encoder.
+                Used in the cross-attention of the decoder
+            decoder_input_ids:
+                Indices of decoder input sequence tokens in the vocabulary.
+
+        Returns:
+            logits on training or output tokens on validation
+        """
+        if encoder_outputs is None:
+            encoder_outputs = self.encoder(pixel_values)
+
+        return self.decoder(
+            encoder_outputs=encoder_outputs,
+            labels=labels,
+            decoder_input_ids=decoder_input_ids
+        )
+
+    def get_encoder(self) -> AutoModelForSeq2SeqLM:
+        """Return model encoder."""
+        return self.encoder
+
+    def prepare_inputs_for_generation(
+        self, input_ids: torch.LongTensor, encoder_outputs: OrderedDict, **kwargs
+    ) -> dict[str, torch.LongTensor | OrderedDict]:
+        """Custom behavior to prepare inputs in the generate method."""
+        return {
+            'decoder_input_ids': input_ids,
+            'encoder_outputs': encoder_outputs
+        }
